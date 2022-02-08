a573330636c143123e14220865beb0612ced8db6
Patrick Asafe <patrickasafe@gmail.com>
2022-02-08 09:57:49 -0300

    first commit

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+import DadosUsuario from "./DadosUsuario";
+
+function FormularioCadastro({aoEnviar, validarCPF}) {
+
+  return (
+    <DadosUsuario/>
+  )
+}
+
+export default FormularioCadastro;
